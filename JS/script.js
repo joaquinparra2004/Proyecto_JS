f311cdebf3f149f3b8b4dd1c664337a1a369bed0
Joaquin Parra <you@example.com>
2022-09-27 19:25:37 -0300

    Tipos de declaraciones de variables

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// tipos de declaraciones de variables 
+
+// VAR - definir variables mutables con un alcance a su funcion mas cercana
+var bestTeam = 'San Lorenzo';
+bestTeam = 'Boca Juniors';
+console.log(bestTeam);
+
+// LET - definir variables mutables donde NO SE PUEDE USAR EL MISMO NOMBRE PARA OTRA VARIABLE, con un alcance a su bloque mas cercano
+let bestStadium = 'Carrefour';
+bestStadium = 'Bombonera';
+console.log(bestStadium);
+
+//  CONST - definir variables imutables con un alcance a su bloque mas cercano
+const bestPlayer = 'Juan Roman Riquelme 10';
+console.log(bestPlayer);
