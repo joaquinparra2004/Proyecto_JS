e049ea5f479a616afe700f854fb73169da7bd472
Joaquin Parra <you@example.com>
2022-10-01 18:03:13 -0300

    ordenando codigo con region

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,27 @@
-// tipos de declaraciones de variables 
-
+//#region Variables
 // VAR - definir variables mutables con un alcance a su funcion mas cercana
 var bestTeam = 'San Lorenzo';
 bestTeam = 'Boca Juniors';
-console.log(bestTeam);
 
 // LET - definir variables mutables donde NO SE PUEDE USAR EL MISMO NOMBRE PARA OTRA VARIABLE, con un alcance a su bloque mas cercano
-let bestStadium = 'Carrefour';
-bestStadium = 'Bombonera';
-console.log(bestStadium);
+let bestStadium = "Carrefour";
+bestStadium = "Bombonera";
 
 //  CONST - definir variables imutables con un alcance a su bloque mas cercano
-const bestPlayer = 'Juan Roman Riquelme 10';
-console.log(bestPlayer);
+const bestPlayer = `Juan Roman Riquelme 10`;
 
-
-
-// Array 
-
-//Creando array 
 let array = ['Boca Juniors', 'River', 'Independiente', 'Racing', 'San Lorenzo'];
+//#endregion Variables
 
+//#region  Console
+console.log(bestTeam);
+console.log(bestStadium);
+console.log(bestPlayer);
+//#endregion Console
+
+//#region  Repetitivas 
 //Recorriendo array  
 for (i = 0; i < 5; i++) {
     console.log(array[i]);
 }
+//#endregion Repetitivas
