dcfb6f8c8ee5d5bf150524658de933b02ddf8afb
Joaquin Parra <you@example.com>
2022-10-02 21:36:19 -0300

    agregando funciones

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,12 @@
 //#region Variables
-// VAR - definir variables mutables con un alcance a su funcion mas cercana
-var bestTeam = 'San Lorenzo';
-bestTeam = 'Boca Juniors';
-
-// LET - definir variables mutables donde NO SE PUEDE USAR EL MISMO NOMBRE PARA OTRA VARIABLE, con un alcance a su bloque mas cercano
-let bestStadium = "Carrefour";
-bestStadium = "Bombonera";
-
-//  CONST - definir variables imutables con un alcance a su bloque mas cercano
-const bestPlayer = `Juan Roman Riquelme 10`;
+var bestTeam = 'Boca Juniors'; // VAR - definir variables mutables con un alcance a su funcion mas cercana
+let bestStadium = "Bombonera"; // LET - definir variables mutables donde NO SE PUEDE USAR EL MISMO NOMBRE PARA OTRA VARIABLE, con un alcance a su bloque mas cercano
+const bestPlayer = `Juan Roman Riquelme 10`; //  CONST - definir variables imutables con un alcance a su bloque mas cercano
 
 let array = ['Boca Juniors', 'River', 'Independiente', 'Racing', 'San Lorenzo'];
+var num1 = 10,
+    num2 = 2;
+
 //#endregion Variables
 
 //#region  Console
@@ -19,9 +15,11 @@ console.log(bestStadium);
 console.log(bestPlayer);
 //#endregion Console
 
-//#region  Repetitivas 
-//Recorriendo array  
+//#region  Repetitivas y Funciones
+
 for (i = 0; i < 5; i++) {
     console.log(array[i]);
 }
+
+
 //#endregion Repetitivas
